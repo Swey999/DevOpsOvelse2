1f4d470e5858528d144aa1496f5b6b43f670a947
mrmobilos <90411669+Swey999@users.noreply.github.com>
2026-02-05 10:35:07 +0100

    Add project files.

---
+++ ConsoleDevOps/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleDevOps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
