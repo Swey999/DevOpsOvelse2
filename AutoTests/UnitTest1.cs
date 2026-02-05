1a3d83046699286145ecdfa93b1f73649a397b87
malt3077 <10503077@ucn.dk>
2026-02-05 12:54:07 +0100

    Testprojekt

---
+++ AutoTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AutoTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void KoererDockerVirkeligVoresTests()
+        {
+            Assert.Pass();
+        }
+    }
+}
